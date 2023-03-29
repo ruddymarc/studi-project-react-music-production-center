9c2f01b6d8d8ad19326b4715141d79b5e08eb99a
Ruddy marc <pc-de-ruddy@LAPTOP-OONQKG13.gp>
2023-03-28 21:19:54 -0400

    style: add google font montserat sans-serif

--- src/components/ui/GlobalStyle.js
+++ src/components/ui/GlobalStyle.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
   body {
+    font-family: 'Montserrat', sans-serif;
     transition: linear .25s;
     background-color: ${(props) => props.theme.backgroundColor};
     color: ${(props) => props.theme.mainColor};
@@ -11,12 +12,13 @@ export default createGlobalStyle`
   h2 { font-size: 1.6rem; }
 
   button {
+    font-family: 'Montserrat', sans-serif;
     transition: linear .25s;
     display: inline-block;
     border-radius: .2em;
     padding: 0.5rem 0;
     margin: 0.5rem 1rem;
-    width: 11em;
+    width: 11rem;
     cursor: pointer;
     border: thin solid;
     background-color: ${(props) => props.theme.backgroundColor};
