61d566af97bf9e31e70d2ae027e94318ec00310b
Ruddy marc <pc-de-ruddy@LAPTOP-OONQKG13.gp>
2023-03-28 21:46:52 -0400

    update app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Layout from "components/ui/Layout";
 import "App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "router";
 
 function App() {
   return (
-    <Layout>
-      <h2>Welcome to MPC app</h2>
-    </Layout>
+    <RouterProvider router={router} />
   );
 }
 
